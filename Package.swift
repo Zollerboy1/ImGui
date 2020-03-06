4144be44584b691a349d3163816d6fc7c7287665
Josef Zoller <josef@walterzollerpiano.com>
2020-03-06 17:23:47 +0100

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -14,15 +14,25 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Zollerboy1/glad.git", from: "1.0.0"),
+        .package(url: "https://github.com/Zollerboy1/GLFW.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
+            name: "cppImGui",
+            dependencies: ["glad", "GLFW"]),
+        .target(
+            name: "cImGui",
+            dependencies: ["cppImGui"]),
+        .target(
             name: "ImGui",
-            dependencies: []),
+            dependencies: ["cImGui"]),
         .testTarget(
             name: "ImGuiTests",
             dependencies: ["ImGui"]),
-    ]
+    ],
+    cLanguageStandard: .c11,
+    cxxLanguageStandard: .cxx11
 )
